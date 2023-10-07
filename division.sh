832e5630232cb3e8906988a5f9f2d02c46d9a66a
ashir245 <ashir.haji@strathmore.edu>
2023-10-07 16:52:21 +0000

    modified division

--- division.sh
+++ division.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo -n 'Enter the First Number: '
+echo -n 'Enter the your first  Number: '
 read a
-echo -n "Enter the Second Number: "
+echo -n "Enter the your second  Number: "
 read b
 echo "$a / $b = $((a / b))"
